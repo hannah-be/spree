7fa7307924867773350b5112f5e900f36a8fb571
Hannah Bourke <hannahbourke0@gmail.com>
2018-06-03 16:01:42 +1000

    Add mongoose

---
+++ api/models/init.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// Use the promise functionality built into Node.js
+mongoose.Promise = global.Promise;
+
+// Connect to our local database
+mongoose
+  .connect("mongodb://localhost/spree", {
+    useMongoClient: true
+  })
+  .then(() => {
+    console.log("Successfully connect to database");
+  })
+  .catch(error => {
+    // If there was an error connect to the database
+    console.error("Error connect to MongoDB databse", error);
+  });
+
+module.exports = mongoose;
